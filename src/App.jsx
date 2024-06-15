23b79a2024d4c25f94a7d8794e8c45e00f2d3ffa
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-15 17:48:00 -0300

    import icons on react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 import './App.css'
 
@@ -6,13 +8,7 @@ function App() {
 
   return (
     <div className='App'>
-      <nav id="navbar">
-        <h2> 
-          <Link to='/'>MoviesLib</Link>
-        </h2>
-        <Link to='movie/1'>Movie</Link>
-        <Link to='search'>Search</Link>
-      </nav>
+      <Navbar />
 
       <h2>Movies Lib</h2>
 
