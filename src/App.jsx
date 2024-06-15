9ff9810e22deb9ec8b1a83ac8de81023b75ef8e7
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-15 19:03:10 -0300

    feat: get movies images and showing at screen

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,9 @@ function App() {
   return (
     <div className='App'>
       <Navbar />
-
-      <h2>Movies Lib</h2>
-
       <Outlet />
     </div>
   )
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 
+
+import MovieCard from "../components/MovieCard";
+
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -24,8 +27,13 @@ const Home = () => {
     }, [])
 
     return(
-        <div>
-            {topMovies && topMovies.map((movie) =>  <p>{movie.title}</p> )}
+        <div className="container">
+            <h2 className="title"> Best Movies</h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>Loading...</p>}
+                {topMovies.length > 0 && 
+                    topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
         </div>
     )
 }
