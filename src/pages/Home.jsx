23d47db8cb7f0ccd961229a1828ead67ee6835e7
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-15 18:25:57 -0300

    fix: location .env

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,28 @@
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
 const Home = () => {
+
+    const [ topMovies, setTopMovies ] = useState([]);
+
+    const getTopRatedMovies = async (url) => {
+        
+        const res = await fetch(url);
+        const data = await res.json();
+
+        console.log(data)
+    }
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+        console.log(topRatedUrl);
+        
+    }, [])
+
     return(
         <h2>Home</h2>
     )
