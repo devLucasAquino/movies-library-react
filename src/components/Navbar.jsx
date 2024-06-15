71175530fb25e519c15392bd8237ac5ac19967d6
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-15 19:55:59 -0300

    feat: import and using useNavigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,35 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import './Navbar.css';
 
 const Navbar = () => {
+    const [ search, setSearch ] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        if(!search) return;
+
+        navigate(`/search?q=${search}`);
+        setSearch('');
+    }
+
+
     return(
     <nav id="navbar">
         <h2> 
           <Link to='/'><BiCameraMovie /> MoviesLib</Link>
         </h2>
-        <form>
-            <input type="text" placeholder="Search an Movie" />
+        <form onSubmit={handleSubmit}>
+            <input  
+                type="text" 
+                placeholder="Search an Movie" 
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+            />
             <button type="submit">
                 <BiSearchAlt2 />
             </button>
