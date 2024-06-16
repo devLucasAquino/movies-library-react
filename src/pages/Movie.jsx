ee132ad8b906406865e74a46abd361b9a9a49da9
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-15 22:55:06 -0300

    complete movie component

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -33,9 +33,44 @@ const Movie = () => {
         getMovie(movieUrl);
     }, [])
 
+    const formatCurrency = (number) => {
+        return number.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+        })
+    }
+
     return(
-        <div>
-            {movie && <>{movie.title}</>}
+        <div className="movie-page">
+            {movie && 
+            <>
+                <MovieCard movie={movie} showLink={false}/>
+                <p className="tagline">{movie.tagline}</p>
+                <div className="info">
+                    <h3>
+                        <BsWallet2 /> Budget:
+                    </h3>
+                    <p>{formatCurrency(movie.budget)}</p>
+                </div>
+                <div className="info">
+                    <h3>
+                        <BsGraphUp /> Revenue:
+                    </h3>
+                    <p>{formatCurrency(movie.revenue)}</p>
+                </div>
+                <div className="info">
+                    <h3>
+                        <BsHourglassSplit /> Duration:
+                    </h3>
+                    <p>{movie.runtime} minutes</p>
+                </div>
+                <div className="info-description">
+                    <h3>
+                        <BsFillFileEarmarkTextFill /> Description:
+                    </h3>
+                    <p>{movie.overview}</p>
+                </div>
+            </>}
         </div>
     )
 }
