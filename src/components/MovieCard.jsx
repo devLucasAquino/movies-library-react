36feb794084f9be3b4ce2327f6767e02e1b68f45
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-17 16:32:16 -0300

    style: update style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,7 +10,7 @@ const MovieCard = ({movie, showLink = true}) => {
             <img src={imageUrl + movie.poster_path} alt={movie.title}/>
             <h2>{movie.title}</h2>
             <p>
-                <FaStar /> {movie.vote_average}
+                <FaStar className="rated"/> {movie.vote_average}
             </p>
             {showLink && <Link to={`/movie/${movie.id}`}>Details</Link>}
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     return(
     <nav id="navbar">
         <h2> 
-          <Link to='/'><BiCameraMovie /> MoviesLib</Link>
+          <Link to='/'><BiCameraMovie /> MoviesLibrary</Link>
         </h2>
         <form onSubmit={handleSubmit}>
             <input  
